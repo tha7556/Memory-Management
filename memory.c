a133c5bd5267b9f459d5ff4274d631fa3533db30
Tyler <tha7556@users.noreply.github.com>
2017-10-25 20:58:35 -0400

    Started work on reference function

--- memory.c
+++ memory.c
@@ -22,14 +22,14 @@ char *toString(FRAME *f)
 
     // For each from that is printed, print the frame number, pcb id of the
     // process that owns it, the page number of the page it contains,
-    // D or C if it is dirty or clean, and the lock count.  
+    // D or C if it is dirty or clean, and the lock count.
 
     if (f == NULL) sprintf (result, "(null) ");
     else sprintf (result, "Frame %d(pcb-%d,page-%d,%c,lock-%d) ",
-               f->frame_id, 
-               f->pcb->pcb_id, 
-               f->page_id, 
-               (f->dirty ? 'D' : 'C'), 
+               f->frame_id,
+               f->pcb->pcb_id,
+               f->page_id,
+               (f->dirty ? 'D' : 'C'),
                f->lock_count);
 
     return result;
@@ -45,6 +45,21 @@ void memory_init()
 
 void reference(int logic_addr, REFER_ACTION action)
 {
+    PCB *pcb = PTBR->pcb;
+    if (______trace_switch) printf("Reference parameters: %d\n\n",logic_addr);
+
+    int pageOffset = log(PAGE_SIZE)/log(2);
+    int pageNumber = log(logic_addr)/log(2);
+    pageNumber = pageNumber - pageOffset;
+    if (______trace_switch) printf("pageNumber: %d\npageOffset %d\n",pageNumber,pageOffset);
+    if(pageNumber >= MAX_PAGE || pageOffset >= MAX_FRAME) { //Not valid pointers
+        Int_Vector.cause = pagefault;
+        Int_Vector.pcb = pcb;
+        Int_Vector.page_id = pageNumber;
+        gen_int_handler();
+
+    }
+
 
 }
 
